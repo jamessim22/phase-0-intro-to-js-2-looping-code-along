3b56bbd850ba870f5f037807df7987d40456a90b
James Simeon <jamessimeon60@gmail.com>
2023-08-02 01:00:21 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+const event = ["Suprise"]
+
+function writeCards(names, event) {
+   return  names.map(name=> `Thank you, ${name}, for the wonderful ${event} gift!`)
+}
+
+console.log(writeCards(names, "birthday"))
+
+function countDown(number) {
+    while (number > 0) {
+        console.log(number);
+        number -= 1;
+    }
+    console.log(number);
+}
